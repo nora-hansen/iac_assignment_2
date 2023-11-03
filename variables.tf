07f035997842b7071ee30d2aeea173a8917030e7
Nora <120593680+nora-hansen@users.noreply.github.com>
2023-11-03 14:34:44 +0100

    Name too long

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 ###### General variables ######
 variable "base_name" {
   type        = string
-  default     = "nhhoperaterra"
+  default     = "nhh"
   description = "value of the base name"
 }
 
